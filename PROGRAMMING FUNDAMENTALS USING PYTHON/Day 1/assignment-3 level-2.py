64b85d6172fa8dfee890d310bb94c52b8c425ef2
ChaitanyaEC <43869613+ChaitanyaEC@users.noreply.github.com>
2021-01-09 21:13:21 +0530

    Create assignment-3 level-2.py

---
+++ PROGRAMMING FUNDAMENTALS USING PYTHON/Day 1/assignment-3 level-2.py
@@ -0,0 +1,21 @@
+#PF-Assgn-3
+#This verification is based on string match.
+
+mileage=12
+amount_per_litre=65
+distance_one_way=96
+per_head_cost=0
+divisible_by_five=False
+
+#Start writing your code from here
+#Populate the variables: per_head_cost and divisible_by_five
+total_distance = distance_one_way * 2
+fuel_price = (total_distance / mileage) * amount_per_litre
+per_head_cost = fuel_price / 4
+if per_head_cost%5==0:
+	divisible_by_five = True
+
+
+#Do not modify the below print statements for verification to work
+print(per_head_cost)
+print(divisible_by_five)
